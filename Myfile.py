ca4c98aab8d475b4a7be4ae08594e776eebe3428
Pedro Perez <96pedroelias96@gmail.com>
2023-06-01 21:42:50 -0500

    m3

--- Myfile.py
+++ Myfile.py
@@ -4,3 +4,11 @@ import torch
 import torch.nn as nn
 import bitsandbytes as bnb
 from transformers import AutoTokenizer, AutoConfig, AutoModelForCausalLM
+
+model = AutoModelForCausalLM.from_pretrained(
+    "facebook/opt-6.7b", 
+    load_in_8bit=True, 
+    device_map='auto',
+)
+
+tokenizer = AutoTokenizer.from_pretrained("facebook/opt-6.7b")
