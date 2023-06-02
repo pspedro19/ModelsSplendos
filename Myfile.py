ed5a92441ec1e2e9c5c96d849f25fba42fa5907c
Pedro Perez <96pedroelias96@gmail.com>
2023-06-01 21:36:25 -0500

    m2

--- Myfile.py
+++ Myfile.py
@@ -1,11 +1,6 @@
-from transformers import GPTNeoXForCausalLM, AutoTokenizer
-tokenizer = AutoTokenizer.from_pretrained("OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5")
-# Note: This model takes 15 GB of Vram when loaded in 8bit
-model = GPTNeoXForCausalLM.from_pretrained(
-  "OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5",device_map="auto", load_in_8bit=True)
-# for cpu ver
-# model = AutoModelForCausalLM.from_pretrained("OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5", torch_dtype=torch.bfloat16)
-message = "<|prompter|>What is a meme, and what's the history behind this word?<|endoftext|><|assistant|>"
-inputs = tokenizer(message, return_tensors="pt").to(model.device)
-tokens = model.generate(**inputs,  max_new_tokens=1000, do_sample=True, temperature=0.8)
-tokenizer.decode(tokens[0])
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
+import torch
+import torch.nn as nn
+import bitsandbytes as bnb
+from transformers import AutoTokenizer, AutoConfig, AutoModelForCausalLM
